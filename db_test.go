13149b68d2415e92bcfcfccfc47521d56dc3656e
witchard <witchard@hotmail.co.uk>
2022-04-16 07:32:42 +0100

    Switched to reader/writer lock

--- db_test.go
+++ db_test.go
@@ -5,13 +5,13 @@ import (
 	"testing"
 )
 
-func TestDBLock(t *testing.T) {
+func TestWriteLock(t *testing.T) {
 	// Use temp dir for goscr files
 	tmp := t.TempDir()
 	os.Setenv("GOSCR_PATH", tmp)
 
 	// Can lock new hash
-	lockA, err := NewProgramLock("abc")
+	lockA, err := LockWrite("abc")
 	if err != nil {
 		t.Error("Couldn't get lock A", err)
 	}
@@ -21,7 +21,7 @@ func TestDBLock(t *testing.T) {
 	defer lockA.Unlock()
 
 	// Fail to lock already locked hash
-	lockB, err := NewProgramLock("abc")
+	lockB, err := LockWrite("abc")
 	if lockB != nil {
 		lockB.Unlock()
 		t.Error("Got lock B and should not have done")
@@ -33,9 +33,22 @@ func TestDBLock(t *testing.T) {
 		t.Error("Unexpected error obtaining lock B", err)
 	}
 
+	// Fail to read lock already locked hash
+	read, err := LockRead("abc")
+	if read != nil {
+		read.Unlock()
+		t.Error("Got read lock and should not have done")
+	}
+	if err == nil {
+		t.Error("Should have received error obtaining read lock")
+	}
+	if err.Error() != "attempt to lock currently locked program" {
+		t.Error("Unexpected error obtaining read lock", err)
+	}
+
 	// Sucessfully lock when other lock is released
 	lockA.Unlock()
-	lockC, err := NewProgramLock("abc")
+	lockC, err := LockWrite("abc")
 	if err != nil {
 		t.Error("Couldn't get lock C", err)
 	}
@@ -43,4 +56,8 @@ func TestDBLock(t *testing.T) {
 		t.Error("Lock C is nil")
 	}
 	defer lockC.Unlock()
+}
+
+func TestReadLock(t *testing.T) {
+	t.Error("TODO: Test multiple read locks can be obtained, and can't get write lock when read lock in place")
 }
