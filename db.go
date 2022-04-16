ccc7b3854b086d5f2b74e0e851416aafe3bbb202
witchard <witchard@hotmail.co.uk>
2022-04-16 07:32:42 +0100

    Switched to reader/writer lock

--- db.go
+++ db.go
@@ -9,22 +9,23 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-type ProgramLock struct {
+type Lock interface {
+	Unlock()
+}
+
+// WriteLock is an exclusive lock (readers or writers) on a program hash
+type WriteLock struct {
 	hash string
 	db   *sql.DB
 }
 
-func (p *ProgramLock) Unlock() {
-	if p != nil && p.db != nil {
-		p.db.Exec(`UPDATE programs SET locked = ?, accessed = ? 
-					WHERE hash = ? AND locked = ?`,
-			false, time.Now().UTC(), p.hash, true)
-		p.db.Close()
-		p.db = nil
-	}
+// ReadLock allows multiple reads on a program hash, but only when no writers
+type ReadLock struct {
+	hash string
+	db   *sql.DB
 }
 
-func NewProgramLock(hash string) (*ProgramLock, error) {
+func openDb() (*sql.DB, error) {
 	dbPath, err := Workdir()
 	if err != nil {
 		return nil, err
@@ -39,20 +40,29 @@ func NewProgramLock(hash string) (*ProgramLock, error) {
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS programs (
 		hash TEXT PRIMARY KEY,
 		accessed DATETIME NOT NULL,
-		locked BOOLEAN NOT NULL
+		readers INTEGER NOT NULL,
+		writers INTEGER NOT NULL
 	)`)
 	if err != nil {
 		db.Close()
 		return nil, err
 	}
+	return db, nil
+}
 
-	_, err = db.Exec("INSERT INTO programs VALUES (?, ?, ?)",
-		hash, time.Now().UTC(), true)
+func LockWrite(hash string) (Lock, error) {
+	db, err := openDb()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec("INSERT INTO programs VALUES (?, ?, ?, ?)",
+		hash, time.Now().UTC(), 0, 1)
 	if err != nil {
 		// Already exists try and lock
-		res, err := db.Exec(`UPDATE programs SET locked = ?, accessed = ? 
-							WHERE hash = ? AND locked = ?`,
-			true, time.Now().UTC(), hash, false)
+		res, err := db.Exec(`UPDATE programs SET writers = ?, accessed = ? 
+							WHERE hash = ? AND writers = ? AND readers = ?`,
+			1, time.Now().UTC(), hash, 0, 0)
 		if err != nil {
 			db.Close()
 			return nil, err
@@ -66,5 +76,50 @@ func NewProgramLock(hash string) (*ProgramLock, error) {
 			return nil, fmt.Errorf("attempt to lock currently locked program")
 		}
 	}
-	return &ProgramLock{hash, db}, nil
+	return &WriteLock{hash, db}, nil
+}
+
+func (l *WriteLock) Unlock() {
+	if l != nil && l.db != nil {
+		l.db.Exec(`UPDATE programs SET writers = ?, accessed = ? 
+					WHERE hash = ? AND writers = ? AND readers = ?`,
+			0, time.Now().UTC(), l.hash, 1, 0)
+		l.db.Close()
+		l.db = nil
+	}
+}
+
+func LockRead(hash string) (Lock, error) {
+	db, err := openDb()
+	if err != nil {
+		return nil, err
+	}
+
+	// Note reads only work for existing hashes (i.e. programs that were created)
+	res, err := db.Exec(`UPDATE programs SET readers = readers + 1, accessed = ? 
+						WHERE hash = ? AND writers = ?`,
+		time.Now().UTC(), hash, 0)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	if rows != 1 {
+		return nil, fmt.Errorf("attempt to lock currently locked program")
+	}
+	return &ReadLock{hash, db}, nil
+}
+
+func (l *ReadLock) Unlock() {
+	if l != nil && l.db != nil {
+		l.db.Exec(`UPDATE programs SET readers = readers - 1, accessed = ? 
+					WHERE hash = ? AND readers > 0 AND writers = ?`,
+			time.Now().UTC(), l.hash, 0)
+		l.db.Close()
+		l.db = nil
+	}
 }
